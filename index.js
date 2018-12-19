36754e09fc6352de41dd2aa13e0afcd0597d953e
Panjapol Sanguanpanich <panjaphol@hotmail.com>
2018-12-20 00:12:30 +0700

    Update Graph

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const fs = require('fs')
-const Json2csvParser = require('json2csv').Parser
-const fields = ['CO', 'Date']
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -11,17 +9,17 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 
-app.post('/api/received_data', (req, res) => {
-    const json2csvParser = new Json2csvParser({ fields })
-    const data = { CO: req.body.msg, Date: req.body.date }
-    const csv = json2csvParser.parse(data)
+app.post('/api/write_data', (req, res) => {
+    let co = ''
+    for (x of req.body.msg) {
+        if (!isNaN(x)) co += x
+    }
+    let csv = '\n' + co + ',' + new Date()
     fs.appendFile('data.csv', csv, function(err) {
         if (err) throw err
         console.log('Saved!')
     })
     res.send(csv)
 })
-
-app.get('/api/sent_data', (req, res) => {})
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
